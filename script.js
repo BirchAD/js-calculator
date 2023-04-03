c03400b1ec2163a66ebb4806ccc5a1644ef29f17
Andrew Birch <andrewbirch1882@icloud.com>
2023-04-03 10:21:39 +0100

    js for calculations if all required fields are complete

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ const peopleInput = document.getElementById('people');
 const personTotal = document.getElementById('person-total');
 const totalTotal = document.getElementById('total-total');
 const resetButton = document.querySelector('.reset');
+
+function calculate() {
+  const bill = parseFloat(billInput.value);
+  const customTip = parseFloat(customTipInput.value);
+  const tipPercent = customTip || parseFloat(document.querySelector('input[name="tip"]:checked').value);
+  const numberOfPeople = parseFloat(peopleInput.value);
+
+  if (bill && tipPercent && numberOfPeople) {
+    const tipAmount = bill * (tipPercent / 100);
+    const totalAmount = bill + tipAmount;
+    const personTip = tipAmount / numberOfPeople;
+    const personTotalAmount = totalAmount / numberOfPeople;
+    // string literal doesnt work?
+    personTotal.textContent = '$' + personTip.toFixed(2);
+    totalTotal.textContent = '$' + personTotalAmount.toFixed(2);
+  } else {
+    // need error messages for 0
+    personTotal.textContent = '$0.00';
+    totalTotal.textContent = '$0.00';
+  }
+}
+
+
+billInput.addEventListener('input', calculate);
+customTipInput.addEventListener('input', calculate);
+tipInputs.forEach(tipInput => tipInput.addEventListener('change', calculate));
+peopleInput.addEventListener('input', calculate);
