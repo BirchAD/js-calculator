18122397b93210bc64b3bdabb7985a2c8e06fd3b
Andrew Birch <andrewbirch1882@icloud.com>
2023-04-03 06:43:59 +0100

    setup js selectors and removed unnecessary id on tip buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const billInput = document.getElementById('amount');
+const tipInputs = document.querySelectorAll('input[name="tip"]');
+const customTipInput = document.getElementById('tipCustom');
+const peopleInput = document.getElementById('people');
+const personTotal = document.getElementById('person-total');
+const totalTotal = document.getElementById('total-total');
+const resetButton = document.querySelector('.reset');
